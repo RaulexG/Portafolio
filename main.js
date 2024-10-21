10527fbe6dc21568cecbc778fd431c330a0e07d4
Raul Chavira Narvaez <166071954+RaulexG@users.noreply.github.com>
2024-10-21 02:06:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//console.log("Hola mundo! desde la consola");
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Ingeniera en sistemas | Desarrollador  Front-end Jr')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¡Si lo puedes imaginar, lo puedes programar!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .pauseFor(2500)
+    .start();
